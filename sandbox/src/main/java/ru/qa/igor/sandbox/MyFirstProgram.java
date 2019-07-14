8caeb07c480877d4790be1c6942718c4976f5c0e
IgorEKuznetsov <igor88@bk.ru>
2019-07-14 03:45:52 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/qa/igor/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/igor/sandbox/MyFirstProgram.java
@@ -3,6 +3,26 @@ package ru.qa.igor.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-      System.out.println("Hello, world!");
+      hello("world!");
+      hello("Igor");
+      hello("Everybody");
+
+      double a = 8;
+      String y = "Площадь квадрата со стороной ";
+      System.out.println(("Площадь квадрата со стороной ")+ a + " = " + area(a));
+
+      System.out.println(y  + a + " = " + area(a));
+      
+
+  }
+      public static void hello(String smt) {
+          System.out.println("Hello, " + smt);
+      }
+
+
+
+
+      public static double area(double a){
+      return a * a;
+      }
  }
-}
